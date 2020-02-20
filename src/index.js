5c444fff144a90446f83b7ae5189a5992019aec2
Rajat Kumar <rajatkumartx@gmail.com>
2020-02-19 22:59:36 -0800

    chore: testing build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import Layout from './component/Layout.jsx';
 class Main extends React.Component {
     render() {
-        return <Layout title="Welcome" />;
+        return (
+            <Layout title="Welcome">
+                <h2>Hello Sugandha!!?</h2>
+            </Layout>
+        );
     }
 }
 
