d1489b668eb0a19cfe72a3fd134e82f37cdce22b
Rajat Kumar <rajatkumartx@gmail.com>
2020-02-19 22:50:23 -0800

    fix: add header

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from './Header.jsx';
 import '../style/main.less';
-const Layout = ({ children }) => {
-    return <div className="container">{children}</div>;
+const Layout = ({ title, children }) => {
+    return (
+        <div className="container">
+            <Header title={title || 'DUMMY'} />
+            {children}
+        </div>
+    );
 };
 
 export default Layout;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import Layout from './component/Layout.jsx';
 class Main extends React.Component {
     render() {
-        return (
-            <Layout>
-                <div className="header">
-                    <h1>Yet Another BoilerPlate</h1>
-                </div>
-            </Layout>
-        );
+        return <Layout title="Welcome" />;
     }
 }
 
